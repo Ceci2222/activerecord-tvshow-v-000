e2dc727eed5f39deae1a7cf29ccc91a1c312c4f1
Marie Burns <mcburns2222@gmail.com>
2019-02-16 04:17:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where("rating = #{self.highest_rating}")[0]
   end
+  
+  def self.lowest_rating
+    Show.minimum("rating")
+  end
+  
+  def self.least_popular_show
+    Show.where("rating = #{self.highest_rating}")[0]
+  end
 end
